5f486c34c23b9ee05cef43779c6649fcc56c3015
Yerson Murga <murgarubioy@iestptrujillo.net>
2024-09-19 11:15:14 -0500

    Mi segundo commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -54,6 +54,7 @@ namespace WebApi.Controllers
         {
             string mensaje = string.Empty;
             Alumno Alumno3 = new Alumno("Edwin", "Paredes", "4370258", new DateTime (1999, 3, 6) );
+            string numero = "siete";
             return Alumno3.DevolverEdad();
         }
 
